77570dce2baf249d501ca0904194a1eafe5c625c
Marround <marround@yandex.ru>
2017-12-31 01:25:38 +0300

    add service for news component

--- src/app/sections/adminka/staticpages/newsedit/newsedit.component.ts
+++ src/app/sections/adminka/staticpages/newsedit/newsedit.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { News } from '../../../../modules/news';
+import { NewsService } from '../../../../service/news.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-newsedit',
   templateUrl: './newsedit.component.html',
   styleUrls: ['./newsedit.component.css']
 })
-export class NewseditComponent implements OnInit {
-
-  constructor() { }
+export class NewseditComponent implements OnInit, OnDestroy {
+  newsArr: News[];
+  newsSub: Subscription;
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
+    this.newsSub = this.newsService.getNews().subscribe(newsArr => {
+      this.newsArr = newsArr;
+    });
   }
-
+  ngOnDestroy() {
+    this.newsSub.unsubscribe();
+  }
 }
